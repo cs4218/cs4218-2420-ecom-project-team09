35b77f7772a45eacaa9d363c6856ae0edab4922a
SimWPEric <e0968924@u.nus.edu>
2025-03-02 11:03:56 +0800

    add unit test for productPhotoController

--- controllers/productController.js
+++ controllers/productController.js
@@ -111,13 +111,13 @@ export const getSingleProductController = async (req, res) => {
 // get photo
 export const productPhotoController = async (req, res) => {
   try {
-    const product = await productModel.findById(req.params.pid).select("photo");
+    const product = await productModel.findOne({ _id: req.params.pid }).select("photo");
     if (product.photo.data) {
       res.set("Content-type", product.photo.contentType);
       return res.status(200).send(product.photo.data);
     }
   } catch (error) {
-    console.log(error);
+    // console.log(error);
     res.status(500).send({
       success: false,
       message: "Error while getting photo",
