c71b5e8684328cc4f7622e89b70e6a8cb6ade5c2
solomonng2001 <solomonng2001@gmail.com>
2025-03-23 14:53:02 +0800

    debug integration tests

--- jest.backend.config.js
+++ jest.backend.config.js
@@ -8,11 +8,15 @@ export default {
 
   // which test to run
   testMatch: [
-    "<rootDir>/controllers/**/*.test.js",
-    "<rootDir>/helpers/*.test.js",
-    "<rootDir>/middlewares/*.test.js",
-    "**/integration/*.test.js",
-    "**/integration/categoryApi.integration.test.js",
+    // "<rootDir>/controllers/**/*.test.js",
+    // "<rootDir>/helpers/*.test.js",
+    // "<rootDir>/middlewares/*.test.js",
+    // "**/integration/*.test.js",
+    // "**/integration/categoryApi.integration.test.js",
+    "**/integration/homepageApi.integration.test.js",
+    "**/integration/categoryproductApi.integration.test.js",
+    "**/integration/productdetailsApi.integration.test.js",
+    "**/integration/searchApi.integration.test.js"
   ],
 
   // jest code coverage
